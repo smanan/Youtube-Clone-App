cdf9aa8929dfd48452f9313a1f85108b6ad317a8
Manan <smanan097@gmail.com>
2023-01-16 16:19:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/system';
+import Navbar from './components/Navbar';
+import ChannelDetail from './components/ChannelDetail';
+import VideoDetail from './components/VideoDetail';
+import Feed from './components/Feed';
+import SearchFeed from './components/SearchFeed';
+// import { Feed } from '@mui/icons-material';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Box sx={{backgroundColor: '#000'}}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed />}/>
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
+}
+
+export default App
