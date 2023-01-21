07f4c97a00f17aa7be559fe6ed5cb480cb89b6ec
Manan <smanan097@gmail.com>
2023-01-21 16:30:39 +0530

    project complete

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Stack } from "@mui/material";
+
 import { categories } from "../utils/constants";
 
-const Sidebar = ({ selectedCatagory, setSelectedCatagory }) => (
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -14,24 +15,17 @@ const Sidebar = ({ selectedCatagory, setSelectedCatagory }) => (
     {categories.map((category) => (
       <button
         className="category-btn"
-        onClick={() => setSelectedCatagory(category.name)}
+        onClick={() => setSelectedCategory(category.name)}
         style={{
-          background: category.name === selectedCatagory && "#FC1503",
+          background: category.name === selectedCategory && "#FC1503",
           color: "white",
         }}
         key={category.name}
       >
-        <span
-          style={{
-            color: category.name === selectedCatagory ? "white" : "red",
-            marginRight: "15px",
-          }}
-        >
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
           {category.icon}
         </span>
-        <span
-          style={{ opacity: category.name === selectedCatagory ? "1" : "0.8" }}
-        >
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
           {category.name}
         </span>
       </button>
@@ -39,4 +33,4 @@ const Sidebar = ({ selectedCatagory, setSelectedCatagory }) => (
   </Stack>
 );
 
-export default Sidebar;
+export default Categories;
